9fd9f786fd8d475452214f0eae4cdddfff1a1acb
Madhav Karavadiya <madhavpatel236@gmail.com>
2025-05-25 19:52:58 +0530

    remove old database connection and update connection placeholder in CrudModel

--- app/Models/CrudModel.php
+++ app/Models/CrudModel.php
@@ -8,7 +8,7 @@ class CrudModel extends Model
 {
     // new_db
     protected $connection =
-    "madhav72";
+    "'// new_db'";
     protected $table = 'crud_table';
     protected $fillable = [
         'Name',
